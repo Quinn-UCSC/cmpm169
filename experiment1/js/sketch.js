586eac8b1760683f22c092859e82d458dc2ad615
Quinn-Hub <56490493+Quinn-Hub@users.noreply.github.com>
2024-01-22 10:02:02 -0800

    Update canvas in exp-1

--- experiment1/js/sketch.js
+++ experiment1/js/sketch.js
@@ -7,61 +7,56 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+let angle = 0;
+let tailAngle = 0;
+let legAngle = 0;
 
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
-    // resize canvas is the page is resized
+    // resize canvas when the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+        resizeCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
+    });	
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+    background(200);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    // Body
+    box(100, 70, 50);
+
+    // Head
+    push();
+    translate(0, -50, 0);
+    rotateY(angle * 2);
+    sphere(40);
+    pop();
+
+    // Legs
+    drawLeg(-30, 30, legAngle);
+    drawLeg(30, 30, -legAngle);
+
+    // Tail
+    push();
+    translate(-70, 10, 0);
+    rotateZ(HALF_PI / 2 + tailAngle);
+    cylinder(10, 100);
+    pop();
+
+    // Animation
+    angle += 0.02;
+    tailAngle = sin(angle) * PI / 4; // Simulates wagging
+    legAngle = sin(angle * 2) * PI / 8; // Simulates walking animation
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function drawLeg(x, y, rotation) {
+    push();
+    translate(x, y, 0);
+    rotateZ(rotation);
+    box(20, 20, 60);
+    pop();
 }
