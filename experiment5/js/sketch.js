cb7e3bf787448887354de9ab4e7650167f32bd51
Quinn-Hub <56490493+Quinn-Hub@users.noreply.github.com>
2024-02-12 23:17:14 -0800

    Experiment 5

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -7,61 +7,51 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+let f = 0;
+let w = 100;
+let d = 50;
+let W = 400;
 
-// Globals
-let myInstance;
 let canvasContainer;
+let rotationSpeed = 0.01; // Initial rotation speed
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
-
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  // Place our canvas, making it fit our container
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
+  canvas.parent("canvas-container");
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  // Resize canvas when the page is resized
+  $(window).resize(function () {
+    resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  });
+
+  w = canvasContainer.height() / 4;
+  d = w / 2;
+  colorMode(HSB, 1); // Set color mode to HSB
 }
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+draw = _ => {
+	background(50);
+	noStroke();
+	[2, 2, -2].map(i => spotLight(W, W, W, 0, -W * i, w, 0, i, 0));
+	rotateY(f += rotationSpeed); // Rotate based on rotationSpeed
+	for (z = -w; z <= w; z += d) {
+		for (y = -w; y <= w; y += d) {
+			for (x = -w; x <= w; x += d) {
+				push();
+				translate(x, y, z);
+				let hue = (frameCount / 100) % 1; // Generate smoothly changing hue
+				let col = color(hue, 1, 1); // Create color using HSB
+				fill(col);
+				sphere((sin(x / w + y / w + z + f * 2) / 16) * (windowHeight));
+				pop();
+			}
+		}
+	}
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+function mouseMoved() {
+    // Change rotation speed based on mouse movement along the X-axis
+    rotationSpeed = map(mouseX, 0, width, -0.1, 0.1);
+  }
